66f09a6a37e5107ce7d00af284469d8858495244
joel-reimann <reimannjoel@gmail.com>
2024-03-08 11:11:56 +0100

    Updated FilterButtons
    The FilterButtons component has been updated for better maintainability. Should make adding future filters easier.

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
-const FilterButtons = ({ filter, setFilter }) => {
-  const buttonClass = (buttonName: string) => 
+const FilterButtons = ({ filter, setFilter }: { filter: string; setFilter: (filter: string) => void }) => {
+  const filters = ['All', 'Active', 'Completed'];
+
+  const createButtonClass = (buttonName: string) => 
     `button ${filter === buttonName ? 'active' : ''}`;
 
   return (
     <div>
-      <button className={buttonClass('All')} onClick={() => setFilter('All')}>Show all</button>
-      <button className={buttonClass('Active')} onClick={() => setFilter('Active')}>Active</button>
-      <button className={buttonClass('Completed')} onClick={() => setFilter('Completed')}>Completed</button>
+      {filters.map(buttonName => (
+        <button 
+          key={buttonName} 
+          className={createButtonClass(buttonName)} 
+          onClick={() => setFilter(buttonName)}
+        >
+          {buttonName}
+        </button>
+      ))}
     </div>
   );
-}
+};
 
 export default FilterButtons;
